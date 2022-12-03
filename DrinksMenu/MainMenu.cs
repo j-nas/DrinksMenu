9327959f66f1f0a9d27dad94613655d133c222a9
Jon Naske <jonnaske@hotmail.com>
2022-12-03 13:54:45 -0800

    Add project files.

---
+++ DrinksMenu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksMenu
+{
+    internal class MainMenu
+    {
+        public MainMenu() 
+        {
+            AnsiConsole.Write(
+                new FigletText("Drink Menu")
+                    .Centered()
+                    .Color(Color.Magenta1)
+                );
+            var selection = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[green]Select from a base liquor[/]")
+                    .PageSize(10)
+                    .AddChoices(new[]
+                    {
+                        "Vodka", "Gin", "Tequila", "Rye", "Rum", "Brandy", "Other"
+                    }));
+
+            AnsiConsole.WriteLine($"i love {selection}");
+        }
+    }
+}
---
+++ DrinksMenu/Program.cs
@@ -0,0 +1,3 @@
+﻿using DrinksMenu;
+
+new MainMenu();
